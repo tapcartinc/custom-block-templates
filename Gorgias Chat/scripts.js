d9d08c07477787391135ed0d5dfe15b13021cc6f
Kenyon Brown <kenyondabrown@gmail.com>
2022-11-30 10:23:36 -0800

    Update scripts.js

---
+++ Gorgias Chat/scripts.js
@@ -0,0 +1,30 @@
+(function() {
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true; 
+    script.onload = function(){
+       GorgiasChat.init().then(function () {
+           
+            GorgiasChat.on('widget:opened', function () {
+                var timer = setInterval(function () {
+                    var chatWindowHead = document.querySelector('#gorgias-chat-container')?.querySelector('#chat-window')?.contentWindow.document.querySelector('head');
+                    if (![...chatWindowHead?.children].some(x => x.getAttribute("data-emotion"))) {
+                        return;
+                    }
+                    clearInterval(timer); 
+
+                    var chatStyle = document.createElement('style');
+                    chatStyle.textContent = '.buttons-wrapper {display: none !important}'; // hide x button
+                    chatWindowHead.appendChild(chatStyle);
+                }, 50);
+            })
+
+         GorgiasChat.open();
+
+        })
+    };
+    // Update the applicationId below
+    script.src = 'https://config.gorgias.chat/gorgias-chat-bundle-loader.js?applicationId=22050';
+    document.querySelector('body').appendChild(script);
+})();
+
