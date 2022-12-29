c98fcb7c4969a547667f2e235e5bc5b9be8159dd
Kenyon Brown <kenyondabrown@gmail.com>
2022-12-28 16:40:42 -0800

    Update scripts.js

---
+++ Fetch Products from the Storefront API/scripts.js
@@ -0,0 +1,34 @@
+const STOREFRONT_ACCESS_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+const GRAPHQL_URL = "https://kenyons-tapcart-boutique.myshopify.com/api/2022-10/graphql.json";
+const productIds = `["gid://shopify/Product/7444358463640"]`;
+const productQuery = () => `
+  query {
+    nodes(ids: ${productIds}) { 
+      ... on Product {
+        id
+        title
+        handle
+        tags
+      }
+    }
+  }
+`;
+
+const GRAPHQL_BODY = () => {
+  return {
+    async: true,
+    crossDomain: true,
+    method: "POST",
+    headers: {
+      "X-Shopify-Storefront-Access-Token": STOREFRONT_ACCESS_TOKEN,
+      "Content-Type": "application/graphql",
+    },
+    body: productQuery(),
+  };
+};
+
+fetch(GRAPHQL_URL, GRAPHQL_BODY())
+  .then((res) => res.json())
+  .then((productResponse) => {
+    console.log(productResponse);
+  });
