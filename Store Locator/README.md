28a09c8cc21e59375f6aa5d1e8145fbf5a28ee44
Jorge <jorge@tapcart.co>
2023-03-18 00:56:30 -0500

    store location sample

---
+++ Store Locator/README.md
@@ -0,0 +1,26 @@
+# Store Locator
+
+## Description
+This block allows customers to find a store near their current geolocation.
+**[REQUIRED]** A developer will need to make updates for this code to work in production. This primarily serves as inpiration for your own stores locator.
+
+## How this block works
+1. This block will ask a user to enable geolocation settings
+2. If permitted it will take their current coordinates and run a lookup
+3. By comparing 3 static store location it will determine the store with the shortest distance
+4. The output will be display at the very top
+
+## Setup Instructions
+> Reach out to your Tapcart rep if you have any questions while configuring this Custom Block
+
+### 1. Create a new Custom Block
+1. Start [here](https://app.tapcart.com/custom-blocks) to create a new Custom Block
+2. Give it a name by clicking on 'Name your block'
+
+### 2. Copy this template's HTML, CSS, and JS over to the Custom Block Editor
+1. Copy the HTML from the [index.html](#) file in this folder, and paste it in the HTML tab in the editor
+2. Copy the CSS from the [styles.css](#) file in this folder, and paste it in the CSS tab in the editor
+3. Copy the Javascript from the [script.js](#) file in this folder, and paste it in the JS tab in the editor
+
+## Block preview
+The store locations will display in the preview, but the full functioning code will work once you're block is dragged and dropped into the tapcart application itself.
---
+++ Store Locator/scripts.js
@@ -0,0 +1,65 @@
+const losAngeles = {
+    lat: 34.0522,
+    lng: -118.2437
+  };
+  const austinTexas = {
+    lat: 30.2672,
+    lng: -97.7431
+  };
+  const miamiFlorida = {
+    lat: 25.7617,
+    lng: -80.1918
+  };
+  
+  function getDistance(lat1, lng1, lat2, lng2) {
+    const R = 6371;
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLng = (lng2 - lng1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLng / 2) *
+        Math.sin(dLng / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d;
+  }
+  
+  function getClosestCity(latitude, longitude) {
+    const laDistance = getDistance(latitude, longitude, losAngeles.lat, losAngeles.lng);
+    const atxDistance = getDistance(latitude, longitude, austinTexas.lat, austinTexas.lng);
+    const mfDistance = getDistance(latitude, longitude, miamiFlorida.lat, miamiFlorida.lng);
+  
+    let closestCity = "";
+    let closestDistance = Infinity;
+  
+    if (laDistance < closestDistance) {
+      closestCity = "Los, Angeles";
+      closestDistance = laDistance;
+    }
+    if (atxDistance < closestDistance) {
+      closestCity = "Austin, Texas";
+      closestDistance = atxDistance;
+    }
+    if (mfDistance < closestDistance) {
+      closestCity = "Miami, Florida";
+      closestDistance = mfDistance;
+    }
+  
+    return closestCity;
+  }
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const closestCity = getClosestCity(
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      document.getElementById("output").innerHTML =
+        "You are closest to " + closestCity;
+    });
+  } else {
+    document.getElementById("output").innerHTML =
+      "Geolocation is not supported by your browser.";
+  }
